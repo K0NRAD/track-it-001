35c78575b7b553972dda6f169008d391ee7db41d
NY1XC <nic.vogt@mail.schwarz>
2022-04-07 14:51:10 +0200

    Create Zeiterfassung.svelte

---
+++ my-svelte-project/src/Zeiterfassung.svelte
@@ -0,0 +1,13 @@
+<script>
+    import newDate from ''
+
+const startButtonClicked = () => {
+    let timeStart = new Date();
+    let dateAndTime = timeStart.getDate() + " " + timeStart.getTime();
+} 
+</script>
+
+
+<div>
+<button class="button" on:click={startButtonClicked() }text="Start work"</button>
+</div>
