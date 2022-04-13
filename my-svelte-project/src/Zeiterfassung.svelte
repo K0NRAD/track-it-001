f666ed1234dda0f6ae6bf883388f9c96ddaf4ca3
NY1XC <nic.vogt@mail.schwarz>
2022-04-13 08:12:30 +0200

    Timetracking
    a

---
+++ my-svelte-project/src/Zeiterfassung.svelte
@@ -0,0 +1,12 @@
+<script>
+
+    const startButtonClicked = () => {
+        let timeStart = new Date();
+        let dateAndTime = timeStart.getDate() + " " + timeStart.getTime();
+    } 
+    </script>
+    
+    
+    <div>
+    <button class="button" on:click={startButtonClicked() }>Start work</button>
+    </div>
