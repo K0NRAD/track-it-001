3a27ef0fe8e4673d4566fd07c1ad51e987192a9d
NY1XC <nic.vogt@mail.schwarz>
2022-04-13 08:10:02 +0200

    Timetracking

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,7 +1,6 @@
 <script>
     import "../node_modules/bulma/css/bulma.min.css";
     import Timetracking from "./components/Timetracking.svelte";
-    import WeeklyFormular from "./components/WeeklyFormular.svelte";
 </script>
 
 <Timetracking/>
